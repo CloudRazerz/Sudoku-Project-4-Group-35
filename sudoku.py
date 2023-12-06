6d5594330f0d43d7d0820167f0a10291a08d4ca0
CloudRazerz <cloudrazerz@gmail.com>
2023-12-06 18:10:19 -0500

    Finished GUI and Game Loop

--- sudoku.py
+++ sudoku.py
@@ -1,5 +1,5 @@
 import pygame
-from generator import SudokuGenerator
+from sudoku_generator import SudokuGenerator
 
 # Initialize Pygame
 pygame.init()
@@ -23,7 +23,8 @@ difficulty = 0
 2 = hard
 """
 board = None
-select = [4, 4, None]  # [x_coordinate, y_coordinate, typed-value]
+default = []
+select = [4, 4, 0, False]  # [x_coordinate, y_coordinate, typed-value, locked or not]
 
 # GUI Elements
 main_font = pygame.font.Font(None, 80)
@@ -80,45 +81,90 @@ def mouse_in_button(mouse_pos, positions, position):
         return False
 
 
-def solvegame(board, i, j):
-    while board[i][j] != 0:
-        if i < 8:
-            i += 1
-        elif i == 8 and j < 8:
-            i = 0
-            j += 1
-        elif i == 8 and j == 8:
-            return True
-    pygame.event.pump()
-    for it in range(1, 10):
-        if  validvalue(board, i, j, it) == True:
-            defaultgrid[i][j] = it
-            global x, z
-            x = i
-            z = j
-            screen.fill((255, 255, 255))
-            drawlines()
-            highlightbox()
-            pygame.display.update()
-            pygame.time.delay(20)
-            if solvegame(defaultgrid, i, j) == 1:
-                return True
-            else:
-                defaultgrid[i][j] = 0
-            screen.fill((0, 0, 0))
+# def solvegame(board, i, j):
+#     while board[i][j] != 0:
+#         if i < 8:
+#             i += 1
+#         elif i == 8 and j < 8:
+#             i = 0
+#             j += 1
+#         elif i == 8 and j == 8:
+#             return True
+#     pygame.event.pump()
+#     for it in range(1, 10):
+#         if  validvalue(board, i, j, it) == True:
+#             defaultgrid[i][j] = it
+#             global x, z
+#             x = i
+#             z = j
+#             screen.fill((255, 255, 255))
+#             drawlines()
+#             highlightbox()
+#             pygame.display.update()
+#             pygame.time.delay(20)
+#             if solvegame(defaultgrid, i, j) == 1:
+#                 return True
+#             else:
+#                 defaultgrid[i][j] = 0
+#             screen.fill((0, 0, 0))
 
-            drawlines()
-            highlightbox()
-            pygame.display.update()
-            pygame.time.delay(50)
-    return False
+#             drawlines()
+#             highlightbox()
+#             pygame.display.update()
+#             pygame.time.delay(50)
+#     return False
+
+def is_full(board):
+    for row in board:
+        for col in row:
+            if col == 0:
+                return False
+    print("hi")
+    return True
+
+def is_correct(board):
+    cols = []
+    box = []
+    rows = []
+    for i in range(len(board)):
+        cols.append(set())
+        for row1 in board:
+            cols[i].add(row1[i])
+
+    
+    track = 0
+    for row_start in range(0,7,3):
+        for col_start in range(0,7,3):
+            box.append(set())
+            for j in range(3):
+                for k in range(3):
+                    box[track].add(board[row_start+j][col_start+k])
+            track += 1
+
+    
+    for l in range(len(board)):
+        rows.append(set())
+        for val in board[l]:
+            rows[l].add(val)
+
+    
+    for check in [cols, box, rows]:
+        for sets in check:
+            if sets == {1,2,3,4,5,6,7,8,9}:
+                continue
+            else:
+                return False
+    return True
+
 def drawlines(board):
-    screen.fill("black")
+    screen.fill("white")
     for i in range(9):
         for j in range(9):
             if board[i][j] != 0:
-
-                pygame.draw.rect(screen, "black", (i * diff, j * diff, diff + 1, diff + 1))
+                if default[i][j] == 1:
+                    pygame.draw.rect(screen, "black", (i * diff, j * diff, diff + 1, diff + 1))
+                if default[i][j] == 2:
+                    pygame.draw.rect(screen, "gray", (i * diff, j * diff, diff + 1, diff + 1))
                 text1 = sub_font.render(str(board[i][j]), 1, (0,255,0))
                 screen.blit(text1, (i * diff + 15, j * diff + 15))
     for l in range(10):
@@ -136,63 +182,75 @@ def cord(pos):
     global z
     z = pos[1]//diff
 
-def fillvalue(value,sketch=True):
-    if sketch:
-        text1 = sub_font.render(str(value), 1, (0, 255, 0))
-    else:
-        text1 = sub_font.render(str(value), 1, (0, 255, 255))
-    screen.blit(text1, (x * diff + 15, z * diff + 15))
+# def fillvalue(value,sketch=True):
+#     value = "" if (value == 0 or value == None) else str(value)
+#     if sketch:
+#         text1 = sub_font.render(value, 1, (0, 255, 0))
+#     else:
+#         text1 = sub_font.render(value, 1, (0, 255, 255))
+#     screen.blit(text1, (x * diff + 15, z * diff + 15))
 
-def highlightbox():
+def highlightbox(color):
+    global x
+    global z
+    if color == "red":
+        x = select[1]
+        z = select[0]
     for k in range(2):
-        #print("x" + str(x))
-        pygame.draw.line(screen, (0, 0, 0), (x * diff - 3, (z + k) * diff), (x * diff + diff + 3, (z + k) * diff), 7)
-        pygame.draw.line(screen, (0, 0, 0), ((x + k) * diff, z * diff), ((x + k) * diff, z * diff + diff), 7)
-    # Game Loop
+        pygame.draw.line(screen, color, (x * diff - 3, (z + k) * diff), (x * diff + diff + 3, (z + k) * diff), 7)
+        pygame.draw.line(screen, color, ((x + k) * diff, z * diff), ((x + k) * diff, z * diff + diff), 7)
+
+
+# Game Loop
 while running:
     keys = pygame.key.get_pressed()
     mouse_pos = pygame.mouse.get_pos()
-    cord(mouse_pos)
-   # screen.fill("white")
-
+    screen.fill("white")
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.KEYDOWN:
             if game_state == 1:
-                select01_temp = select[0:2]
+                select1_temp = select[0:2]
                 if event.key == pygame.K_LEFT or event.key == pygame.K_a:
-                    if select[0] > 0:
-                        select[0] -= 1
-                        select[2] = None
-                if event.key == pygame.K_RIGHT or event.key == pygame.K_d:
-                    if select[0] < 8:
-                        select[0] += 1
-                        select[2] = None
-                if event.key == pygame.K_UP or event.key == pygame.K_w:
                     if select[1] > 0:
+                        select[3] = False
                         select[1] -= 1
                         select[2] = None
-                if event.key == pygame.K_DOWN or event.key == pygame.K_s:
+                if event.key == pygame.K_RIGHT or event.key == pygame.K_d:
                     if select[1] < 8:
+                        select[3] = False
                         select[1] += 1
                         select[2] = None
-                if select01_temp != select[0:2]:
+                if event.key == pygame.K_UP or event.key == pygame.K_w:
+                    if select[0] > 0:
+                        select[3] = False
+                        select[0] -= 1
+                        select[2] = None
+                if event.key == pygame.K_DOWN or event.key == pygame.K_s:
+                    if select[0] < 8:
+                        select[3] = False
+                        select[0] += 1
+                        select[2] = None
+                if select1_temp != select[0:2]:
                     print("selected", select[0:2])
+                    x = select[1]
+                    z = select[0]
+                    select[3] = True
                 if event.key == pygame.K_RETURN:
-                    if select[2] != None:
-                        # board.place_number(select[2])
+                    if select[2] != 0:
                         print("confirmed", select[2], "at", select[0:2])
+                        select[3] = False
+                        default[select[1]][select[0]] = 2
+                        if is_full(board):
+                            if is_correct(board):
+                                game_state = 3
+                            else:
+                                game_state = 2
 
-                        fillvalue(select[2],True)
-
-                        # if board.is_full():
-                        #    if board.check_board():
-                        #        game_state = 3
-                        #    else:
-                        #        game_state = 2
-                    continue
                 select2_temp = select[2]
+                if event.key == pygame.K_BACKSPACE or event.key == pygame.K_0 or event.key == pygame.K_KP_0:
+                    select[2] = 0
                 if event.key == pygame.K_1 or event.key == pygame.K_KP_1:
                     select[2] = 1
                 if event.key == pygame.K_2 or event.key == pygame.K_KP_2:
@@ -212,10 +270,9 @@ while running:
                 if event.key == pygame.K_9 or event.key == pygame.K_KP_9:
                     select[2] = 9
                 if select2_temp != select[2] and select[2] != None:
-                    # board.sketch(select[2])
+                    if default[select[1]][select[0]] == 0:
+                        board[select[1]][select[0]] = select[2]
                     print("sketched", select[2], "at", select[0:2])
-                    fillvalue(select[2],False)
-                    select[2] = None
 
     # Main Menu
     if game_state == 0:
@@ -227,23 +284,28 @@ while running:
                 screen.blit(difficulty_positions[position][1], difficulty_positions[position][0])
                 if event.type == pygame.MOUSEBUTTONUP:
                     difficulty = 0 if position == easy else 1 if position == medium else 2
-                    # board = Board(screen.get_width(), screen.get_height()-60, screen, difficulty)
-                    select = [4, 4, None]
+                    select = [4, 4, 0, False]
                     print("selected", select[0:2])
                     # board.select(4,4)
                     if difficulty == 1:
                         sg = SudokuGenerator(9, 40)
-
                         board = sg.generate_sudoku(9, 40)
-                        #print("Pringing board" + str(board))
-
-
                     elif difficulty == 0:
                         sg = SudokuGenerator(9, 30)
                         board = sg.generate_sudoku(9, 30)
                     elif difficulty == 2:
                         sg = SudokuGenerator(9, 50)
                         board = sg.generate_sudoku(9, 50)
+                    sg = SudokuGenerator(9, 3)
+                    board = sg.generate_sudoku(9, 3)
+                    default = []
+                    for i in range(len(board)):
+                        default.append([])
+                        for value in board[i]:
+                            if value != 0:
+                                default[i].append(1)
+                            else:
+                                default[i].append(0)
                     drawlines(board)
 
                     game_state = 1
@@ -253,19 +315,35 @@ while running:
 
     # In Game
     if game_state == 1:
+        cord(mouse_pos)
+        drawlines(board)
+        if (mouse_pos[0] > 0 and mouse_pos[1] > 0
+                    and mouse_pos[0] < 540 and mouse_pos[1] < 540):
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                select[3] = False
+                select[0] = z
+                select[1] = x
+                select[2] = 0
+                select[3] = True
+        if not select[3]:
+            highlightbox("black")
+        else:
+            highlightbox("red")
 
-        # board.update_board()
-        # board.draw()
+
+        #Display bottom 3 buttons and functionality
         pygame.draw.rect(screen, "gray", (0, screen.get_height() - 60, screen.get_width(), 60))
         for position in game_button_positions:
             if mouse_in_button(mouse_pos, game_button_positions, position):
                 screen.blit(game_button_positions[position][1], game_button_positions[position][0])
                 if event.type == pygame.MOUSEBUTTONDOWN:
-
                     # Press Reset
                     if position == reset:
-                        # board.reset_to_original()
-                        pass
+                        for rowindex in range(len(board)):
+                            for colindex in range(len(board[rowindex])):
+                                if default[rowindex][colindex] == 0 or default[rowindex][colindex] == 2:
+                                    board[rowindex][colindex] = 0
+                                    select[3] = False
 
                     # Press Restart
                     elif position == restart:
@@ -277,19 +355,7 @@ while running:
             else:
                 screen.blit(position, game_button_positions[position][0])
 
-        # Click Select
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if (mouse_pos[0] > 0 and mouse_pos[1] > 0
-                    and mouse_pos[0] < 540 and mouse_pos[1] < 540):
-                cord(mouse_pos)
-               # print("clicked at", mouse_pos)
-                # if board.click(mouse_pos[0], mouse_pos[1]) != None:
-                #    select[0] = board.click(mouse_pos[0], mouse_pos[1])[0]
-                #    select[1] = board.click(mouse_pos[0], mouse_pos[1])[1]
-                #    select[2] = None
-                #pass
-        if select[2] is None:
-            highlightbox()
+
     # Game Over
     if game_state == 2:
         screen.blit(game_over, (screen.get_width() / 2 - game_over.get_width() / 2, screen.get_height() / 4))
@@ -311,21 +377,5 @@ while running:
             screen.blit(exit_alt, won_positions[exit_alt][0])
 
     pygame.display.update()
-    #pygame.display.flip()
-
-def drawlines(board):
-    for i in range (9):
-        for j in range (9):
-            if board[i][j]!= 0:
-                pygame.draw.rect(screen, (255, 255, 0), (i * diff, j * diff, diff + 1, diff + 1))
-                text1 = sub_font.render(str(board[i][j]), 1, (0, 0, 0))
-                screen.blit(text1, (i * diff + 15, j * diff + 15))
-    for l in range(10):
-        if l % 3 == 0 :
-            thick = 7
-        else:
-            thick = 1
-        pygame.draw.line(screen, (0, 0, 0), (0, l * diff), (500, l * diff), thick)
-        pygame.draw.line(screen, (0, 0, 0), (l * diff, 0), (l * diff, 500), thick)
 
 pygame.quit()
