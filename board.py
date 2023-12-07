ae0f9c66ad75a2f30c0f004d5f3636663949d486
raidensaunders <raiden.saunders@ufl.edu>
2023-12-06 20:26:25 -0500

    Cell and Board classes with comments

--- board.py
+++ board.py
@@ -2,26 +2,26 @@ import pygame, sys
 from cell import Cell
 
 class Board:
-    selected = None
+    selected = None #creates empty class attribute to store the x and y coordinates of the where selected on the board
 
     def __init__(self, width, height, screen, difficulty):
         self.width = width
         self.height = height
         self.screen = screen
         self.difficulty = difficulty
-        self.cells = []
+        self.cells = [] # creates empty list that will store rows as iterated by the next for loop
         for i in range(9):
-            self.cells.append([])
+            self.cells.append([]) #creates row values in self.cells list
             for j in range(9):
-                self.cells[i].append(Cell(0, i, j, screen))
-        pygame.init() #Dont know if this goes here or not but we will see
+                self.cells[i].append(Cell(0, i, j, screen)) #goes into each row variable and puts in list of corresponding cells
+        pygame.init()
 
-    def draw(self):
-        for row in self.cells:
+    def draw(self):# draw functon that draws the entirety of the board
+        for row in self.cells: #nested for loop that draws grid for each cell so that each cell has own surface
             for cell in row:
                 cell.draw()
-        for j in range(0,600,60):
-            for k in range(0,600,60):
+        for j in range(0,600,60): #draws the cell grid
+            for k in range(0,600,60): #draws horizontal lines
                 for i in range(0, 2):
                     pygame.draw.line(
                     self.screen,
@@ -30,7 +30,7 @@ class Board:
                         (k, i * self.screen.get_width()/9 + j)
                     )
 
-                for i in range(0,2):
+                for i in range(0,2): #draws vertical
                     pygame.draw.line(
                         self.screen,
                         'gray',
@@ -38,7 +38,7 @@ class Board:
                         (i *self.screen.get_width()/9 + j, k)
                     )
 
-        for i in range(1, 3):
+        for i in range(1, 3): #draws horizontal lines for the large box grid
             pygame.draw.line(
             self.screen,
             'black',
@@ -47,7 +47,7 @@ class Board:
                 3
             )
 
-        for i in range(1,3):
+        for i in range(1,3): #draws vertical lnes for the large box grid
             pygame.draw.line(
                 self.screen,
                 'black',
@@ -57,32 +57,34 @@ class Board:
             )
         pass
 
-    def select(self, row, col):
-        self.selected = self.cells[row][col]
+    def select(self, row, col): #assigns the selected class attribute to the cell coordinates
+        self.selected = self.cells[row][col] #assigns selected the cell coordinates which are the row and col index numbers in self.cells
 
 
     def click(self, x, y):
+        # sets row and col through taking the coordinates of the mouse click and dividing by 60 to return number 1-9
+        #referring to how there are 9 columns and rows on the sudoku board
         row = y//60
         col = x//60
         return int(row), int(col)
 
-    def clear(self):
-        if self.selected != None:
-            if self.selected.default == False:
-                self.selected.set_sketch_value(0)
+    def clear(self): #clears board when reset is used
+        if self.selected != None: #checks to make sure that a cell is selected
+            if self.selected.default == False: #sees if cell is at default value
+                self.selected.set_sketch_value(0) #if cell is not at default then sets back to default by setting to 0
 
-    def sketch(self, value):
+    def sketch(self, value): #puts sketh value on the board
         self.value = value
-        if self.selected != None:
-            self.selected.set_sketched_value()
+        if self.selected != None: #checks to make sure the cell is empty
+            self.selected.set_sketched_value() #if empty sets the value entered by the user
 
 
-    def place_number(self, value):
+    def place_number(self, value): #confirms the sketch value
         self.value = value
-        if self.confirmed != None:
-            self.selected.set_value()
+        if self.confirmed != None: #checks to see i empty
+            self.selected.set_value() #if empty sets the value
 
-    def is_full(self):
+    def is_full(self): #checks to make sure the board is full so can see whether win or lose
         for row in self.cells:
             for cell in row:
                 if cell.confimred:
@@ -91,37 +93,38 @@ class Board:
                     return False
         return True
 
-    def check_board(self):
-        board =[]
+    def check_board(self): #checks to see that every box, row, and column only has one of each number
+        #each of these lists coresponds to the rows, columns, and boxes of the board and the numebr values within them
+        board =[] #converts each cells into their number values so the board can be properly checked
         rows = []
         col = []
         box = []
-        for row in range(len(self.cells)):
+        for row in range(len(self.cells)): #creates list
             board.append([])
             for cell in self.cells[row]:
                 board[row].append(cell.value)
-        for value in board:
+        for value in board: #takes values within board and puts the value in each cell to its corresponding row
             rows.append(set(value))
 
-        for i in range(len(board)):
+        for i in range(len(board)): #does same except for the columns
             col.append(set())
             for row in board:
                 col[i].add(row[i])
 
         coord = [(0,0),(0,3),(0,6),(3,0),(3,3),(3,6),(6,0),(6,3),(6,6)]
-        for top in range(len(coord)):
-            box.append(set())
+        for top in range(len(coord)): #the hardest one to program, uses the list above of the coordinates of the top left of each box
+            box.append(set()) #appends to box as sets
             for j in range(3):
                 for k in range(3):
-                    box[top].add(board[coord[top][0]+j][coord[top][1]+k])
-        for check in [col, rows, box]:
+                    box[top].add(board[coord[top][0]+j][coord[top][1]+k]) #goes through and adds the cell values to each box in the box list
+        for check in [col, rows, box]: #fnally goes through columns, rows, and boxes and checks to make sure has 1-9 without repeats in the same row, column, or boxes
             for sets in check:
                 if sets == {1,2,3,4,5,6,7,8,9}:
                     continue
                 else:
-                    return False
+                    return False #if repeats, returns False
 
-        return True
+        return True #if no repeats, returns true
 
 
 
--- cell.py
+++ cell.py
@@ -1,27 +1,28 @@
 import pygame, sys
 class Cell:
-    def __init__(self, value, row, col, screen):
+    def __init__(self, value, row, col, screen): #initializes the parameters of the Cell class
         self.value = value
         self.row = row
         self.col = col
         self.screen = screen
-        self.confirmed = False
-        self.default = None
+        self.confirmed = False #Boolean variable to check if there is a value in the cell
+        self.default = None #Boolean to see if the cell started empty or is one of the filled cells
 
-    def set_cell_value(self, value):
-        if self.confirmed == False:
-            self.value = value
-            self.confirmed = True
+    def set_cell_value(self, value): #sets the value for the cell
+        if self.confirmed == False: #first checks if confirm false (if so cell empty
+            self.value = value #then sets the value in that cell
+            self.confirmed = True #changes confirmed from fale to true so the value cant be changed
         
-    def set_sketched_value(self, value):
-        if self.confirmed == False:
-            self.value = value
+    def set_sketched_value(self, value): #repeat of the function above except doesn't confirms the value
+        if self.confirmed == False: #if cell empty...
+            self.value = value #then set place holder value in the cell
+            #does not confirm value as the answer
 
 
     def draw(self):
-        for j in range(0,600,60):
+        for j in range(0,600,60): # nested for loops here work to draw the cell grid for pygame
             for k in range(0,600,60):
-                for i in range(0, 2):
+                for i in range(0, 2): #draws the horizontal lines
                     pygame.draw.line(
                     self.screen,
                     'gray',
@@ -29,13 +30,14 @@ class Cell:
                         (k, i * self.screen.get_width()/9 + j)
                     )
 
-                for i in range(0,2):
+                for i in range(0,2): #draws the vertical lines
                     pygame.draw.line(
                         self.screen,
                         'gray',
                         (j, k),
                         (i *self.screen.get_width()/9 + j, k)
                     )
+        #Sets the font for the numbers to be entered in the cells
         flont = pygame.font.Font(None, size=30)
         sketch = flont.render(str(self.value), True, 'gray', None)
         self.screen.blit(sketch, (self.col * 60 + 5, self.row * 60))
